79eceb015b0d536f6441a8b184d4cb2043d99667
unknown <1309458652@qq.com>
2022-08-19 16:31:10 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     version = '0.1.6',
     keywords='buildz',
     long_description=open('README.md', 'r', encoding="utf-8").read(),
+    long_description_content_type='text/markdown'
     description = "a json-like file format's read and write code by python, and codes to read and product object from configure file in such format",
     license = 'Apache License 2.0',
     url = 'https://github.com/buildCodeZ/buildz',
