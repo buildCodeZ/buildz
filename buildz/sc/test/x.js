4dc34aa394218eca7e0df0254e6d65b2152cdb4d
buildCodeZ <1309458652@qq.com>
2025-04-30 00:32:20 +0800

    sc update

--- buildz/sc/test/x.js
+++ buildz/sc/test/x.js
@@ -1,5 +1,9 @@
 
 
 dp: ''
-fps: [x.py]
+fps: [x.py, inc.py]
 run: x.test
+args: []
+stdin: std // std, [file, ...], [str, ....]
+stdout: std//[file, out.txt] // std, [file, ...]
+stderr: std//[file, out.txt]
